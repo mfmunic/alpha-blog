7731ed20f9f367cc3b57720ec533cc7203c9f517
Marc Munic <mfmunic@gmail.com>
2026-01-29 15:40:55 -0800

    added super admin func

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,24 +39,24 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to articles_path
   end
 
   private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def require_same_user
-      if current_user != @user
-        flash[:alert] = "You can only edit your own account"
-        redirect_to @user
-      end
+  def require_same_user
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
+      redirect_to @user
     end
+  end
 end
