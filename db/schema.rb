95b9c4ba46f8ba45bee272e2dbbcd2081a87fe5c
Marc Munic <mfmunic@gmail.com>
2026-02-02 13:13:04 -0800

    added cateogory many to many; and display categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_30_172301) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_02_181637) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
